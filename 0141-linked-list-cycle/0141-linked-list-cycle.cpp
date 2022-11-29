8b784e0bb2e197aa693fb1f6f16ccf748897449a
Bhargav Valani <62599424+Bhargav-2808@users.noreply.github.com>
2022-11-29 11:45:57 +0530

    Time: 17 ms (72.49%), Space: 7.9 MB (81.31%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,7 +9,7 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        map<ListNode*, bool> visited;
+        /*map<ListNode*, bool> visited;
         
         if(head == NULL)
         {
@@ -26,7 +26,32 @@ public:
             
             visited[temp] = true;
             temp = temp ->next;
+        }*/
+        //floyed detection
+        ListNode* slow = head;
+        ListNode* fast = head;
+        
+        if(head == NULL || head->next == NULL )
+        {
+            return false;
         }
+        
+        while(slow!=NULL && fast!=NULL)
+        {
+            fast= fast ->next;
+            if(fast!=NULL)
+            {
+                fast= fast->next;
+            }
+            slow=slow->next;
+            
+            if(slow==fast)
+            {
+                return true;
+            }
+        }
+        
+        
         return false;
     }
 };
