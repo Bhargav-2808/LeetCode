15fe2375ab1306daa8ccc9d008b32f6acb9cfb71
Bhargav Valani <62599424+Bhargav-2808@users.noreply.github.com>
2022-12-08 13:45:51 +0530

    Time: 5 ms (33.71%), Space: 8.3 MB (71.33%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        int i= 0;
+        int j = nums.size()-1;
+        int pivot = 0;
+        
+        while(pivot<=j)
+        {
+            if(nums[pivot] == 0)
+            {
+                swap(nums[i++],nums[pivot++]);
+            }
+            else if(nums[pivot] == 1 )
+            {
+                pivot++;
+            }
+            else if(nums[pivot] == 2)
+            {
+                swap(nums[j--],nums[pivot]);
+            }
+        }
     }
 };
