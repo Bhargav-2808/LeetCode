9ee2d2b13a855d0d18f724825853506baa8a5b03
Bhargav Valani <62599424+Bhargav-2808@users.noreply.github.com>
2022-12-27 13:27:36 +0530

    Time: 12 ms (73.08%), Space: 18.9 MB (15.92%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -17,9 +17,8 @@ public:
         if(root == NULL){
             return 0;
         }
-        int left = maxDepth(root->left);        
-        int right = maxDepth(root->right); 
+
  
-        return max(left,right) +1;
+        return max(maxDepth(root->left),maxDepth(root->right)) +1;
     }
 };
